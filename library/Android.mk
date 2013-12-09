0c6f66e9880cb29feefdc6cc211e72eff0e9287e
Chris Soyars <ctso@ctso.me>
2013-12-09 12:12:42 -0800

    Include libcurve25519 with TextSecure library
    Change-Id: Ib352ae9bd5580a4b98b5b5027f1e80522dd23b92

--- library/Android.mk
+++ library/Android.mk
@@ -17,6 +17,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_STATIC_JAVA_LIBRARIES := libphonenumbergoogle thoughtcrimegson spongycastle-light-jdk15on protobuf-java-2.4.1
 
 LOCAL_STATIC_LIBRARIES := libcurve25519-donna libcurve25519
+LOCAL_ADDITIONAL_DEPENDENCIES += libcurve25519
 
 include $(BUILD_STATIC_JAVA_LIBRARY)
 
@@ -28,3 +29,5 @@ LOCAL_MODULE_CLASS := JAVA_LIBRARIES
 LOCAL_MODULE_PATH := $(TARGET_OUT)/fake_packages/$(LOCAL_SRC_FILES)
 
 include $(BUILD_PREBUILT)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
--- library/jni/Android.mk
+++ library/jni/Android.mk
@@ -10,6 +10,7 @@ include $(BUILD_STATIC_LIBRARY)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := libcurve25519
+LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := curve25519-donna-jni.c
 
 LOCAL_STATIC_LIBRARIES := libcurve25519-donna
